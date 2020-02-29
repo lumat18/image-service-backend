45306b3851e8b8f2d61fdb285a4aae100dffb350
lumat <lumat@op.pl>
2020-02-29 21:05:21 +0100

    add photo entity

---
+++ src/main/java/com/photoarchive/models/Photo.java
@@ -0,0 +1,32 @@
+package com.photoarchive.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity(name = "photos")
+public class Photo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long photo_id;
+    @Column(nullable = false)
+    private String href;
+    @ManyToMany
+    @JoinTable(
+            name = "photos_tags",
+            joinColumns = @JoinColumn(name = "photo_id"),
+            inverseJoinColumns = @JoinColumn(name = "tags_id")
+    )
+    private Set<Tag> tags;
+
+    public Photo(String href, Set<Tag> tags) {
+        this.href = href;
+        this.tags = tags;
+    }
+}
---
+++ src/main/java/com/photoarchive/models/Tag.java
@@ -0,0 +1,10 @@
+package com.photoarchive.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Tag {
+    @Id
+    private Long id;
+}
