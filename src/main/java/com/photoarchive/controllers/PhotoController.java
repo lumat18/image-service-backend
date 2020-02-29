23478c3fc033efa28b5a51d1ad8e66b7049baeea
lumat <lumat@op.pl>
2020-02-29 21:37:22 +0100

    add getPhotoByTag + StringValidatorService

--- src/main/java/com/photoarchive/controllers/PhotoController.java
+++ src/main/java/com/photoarchive/controllers/PhotoController.java
@@ -2,24 +2,37 @@ package com.photoarchive.controllers;
 
 import com.photoarchive.models.Photo;
 import com.photoarchive.repositories.PhotoRepository;
+import com.photoarchive.services.StringValidatorService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/photos")
 public class PhotoController {
 
     private PhotoRepository photoRepository;
+    private StringValidatorService stringValidatorService;
 
-    public PhotoController(PhotoRepository photoRepository) {
+    public PhotoController(PhotoRepository photoRepository, StringValidatorService stringValidatorService) {
         this.photoRepository = photoRepository;
+        this.stringValidatorService = stringValidatorService;
     }
 
     @GetMapping
     public List<Photo> getAllPhotos(){
         return photoRepository.findAll();
+    }
+
+    @GetMapping
+    public List<Photo> getPhotosByTag(@RequestParam(name = "tag") String tag){
+        final String tagInput = stringValidatorService.handleTagInput(tag);
+        return photoRepository.findAll().stream()
+                .filter(photo -> photo.getTags().contains(tagInput))
+                .collect(Collectors.toList());
     }
 }
