c59dfc6ee3186c8692704cc36edb5939337b71b1
lumat <lumat@op.pl>
2020-03-04 20:49:48 +0100

    add CORS configutaion to main class

--- src/main/java/com/photoarchive/PhotoArchiveApplication.java
+++ src/main/java/com/photoarchive/PhotoArchiveApplication.java
@@ -2,6 +2,9 @@ package com.photoarchive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PhotoArchiveApplication {
@@ -10,4 +13,13 @@ public class PhotoArchiveApplication {
         SpringApplication.run(PhotoArchiveApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/photos").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 }
